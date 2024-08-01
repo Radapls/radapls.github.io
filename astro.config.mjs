c979a7bd1062f9a570cc201ffa49535ba2b72413
Juan Felipe Rada <radapls8@gmail.com>
2024-08-01 12:26:04 -0300

    JFRA-00: Fix identation, add ignore.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,31 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+const isProd = import.meta.env.PROD;
+const isDev = import.meta.env.DEV;
+const SERVER_PORT = 8888;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}/`;
+const PROD_URL = 'https://radapls.github.com';
+
+let BASE_URL;
+if (isProd)
+{
+    BASE_URL = PROD_URL;
+} else if (isDev)
+{
+    BASE_URL = LOCALHOST_URL;
+}
+
+/**
+ * This are the current configurations for the project
+ * More info: https://astro.build/config
+ */
+export default defineConfig({
+    site: BASE_URL,
+    server: {
+        port: SERVER_PORT,
+        host: true
+    },
+    prefetch: {
+        prefetchAll: true
+    }
+});
