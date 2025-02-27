b94c6cb2a7fc09b1e56b6bf4a7be9c37a361dd11
Juan Felipe Rada <radapls8@gmail.com>
2025-02-27 16:53:14 -0500

    JFRA-07: Refactor Skills section and add some articles.

---
+++ src/data/skillIcons.ts
@@ -0,0 +1,20 @@
+export const SkillIcons = [
+    { icon: "devicon-javascript-plain", link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
+    { icon: "devicon-html5-plain", link: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
+    { icon: "devicon-css3-plain", link: "https://developer.mozilla.org/en-US/docs/Web/CSS" },
+    { icon: "devicon-typescript-plain", link: "https://www.typescriptlang.org/" },
+    { icon: "devicon-angular-plain", link: "https://angular.dev/" },
+    { icon: "devicon-react-original", link: "https://react.dev/" },
+    { icon: "devicon-sass-original", link: "https://sass-lang.com/" },
+    { icon: "devicon-astro-plain", link: "https://astro.build/" },
+    { icon: "devicon-bootstrap-plain", link: "https://getbootstrap.com/" },
+    { icon: "devicon-gatsby-original", link: "https://www.gatsbyjs.com/" },
+    { icon: "devicon-figma-plain", link: "https://www.figma.com/" },
+    { icon: "devicon-ngrx-plain", link: "https://ngrx.io/" },
+    { icon: "devicon-rxjs-plain", link: "https://rxjs.dev/" },
+    { icon: "devicon-redux-plain", link: "https://redux.js.org/" },
+    { icon: "devicon-jira-plain", link: "https://jira.atlassian.com/" },
+    { icon: "devicon-git-plain", link: "https://git-scm.com/" },
+    { icon: "devicon-github-original", link: "https://github.com/" },
+    { icon: "devicon-storybook-plain", link: "https://storybook.js.org/" }
+  ];
